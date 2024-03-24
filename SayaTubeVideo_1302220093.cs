43be4a81b627294c44fb167763408b2bffc2da20
Faisal Ramadhan <rdmfaisalrj@student.telkomuniversity.ac.id>
2024-03-24 22:49:59 +0700

    menambahkan class SayaTubeVideo

---
+++ SayaTubeVideo_1302220093.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD06_1302220093
+{
+    public class SayaTubeVideo_1302220093
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo_1302220093(string title)
+        {
+            // Kode tersebut menggunakan kelas Random untuk menghasilkan angka acak antara 9999 dan 100001,
+            // yang kemudian disimpan dalam variabel "id".
+            Random acak = new Random();
+            id = acak.Next(9999, 100001);
+
+            //  Precondition
+            // Memastikan bahwa panjang string title tidak lebih dari 100 karakter dan
+            // bahwa title tidak null sebelum mengatur nilainya.
+            Debug.Assert(title.Length <= 100 && title != null);
+            this.title = title;
+
+            // state awal playCount bernilai 0
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            
+        }
+
+        // Print detail informasi dari Video
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"Video ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
--- obj/Debug/net8.0/TPMOD06_1302220093.AssemblyInfo.cs
+++ obj/Debug/net8.0/TPMOD06_1302220093.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPMOD06_1302220093")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02962ade4f815136773836c7f806a5b6a67caf80")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPMOD06_1302220093")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPMOD06_1302220093")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
